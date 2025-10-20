2c3f12cad4e130d45275262ed6d3f6de69422954
JoshuaEllis13 <121315866+Joshcode12@users.noreply.github.com>
2025-10-20 16:49:23 -0600

    Add all the project files.

---
+++ Firmware/main.c
@@ -0,0 +1,20 @@
+/*********************************************************************
+ * File: main.c
+ * Purpose : Main entry point of the program
+ * Author  : Joshua
+ *********************************************************************/
+
+#include <stdio.h>
+
+/**
+ * @brief Main entry point of the program
+ */
+int main(void) {
+  int i;
+
+  for (i = 0; i < 100; i++) {
+    printf("Hello World %d!\n", i + 1);
+  }
+}
+
+/*************************** End of file ****************************/
